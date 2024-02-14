183f296de954c268bc90aee9c65468aa503eece3
griphaugland <griphaugland@gmail.com>
2024-02-14 16:41:35 +0100

    Added delete and patch

--- index.js
+++ index.js
@@ -39,6 +39,113 @@ pool.connect((err, client, release) => {
   });
 });
 
+// PATCH + DELETE USERS
+app.delete("/users", async (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  try {
+    const verifiedToken = jwt.verify(token, process.env.TOKEN_HASH_KEY);
+    if (verifiedToken) {
+      const { users } = await pool.query("DELETE FROM users WHERE id = $1;", [
+        verifiedToken.id,
+      ]);
+      const { addictionItems } = await await pool.query(
+        "DELETE FROM addiction_items WHERE user_id = $1;",
+        [verifiedToken.id]
+      );
+      res.json({ message: "Deleted your account, its a shame to see you go!" });
+    } else {
+      res.status(404).json({
+        message: "Invalid token",
+      });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(`Server error: ${err.message}`);
+  }
+});
+
+app.patch("/users", async (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const {
+    username,
+    email,
+    password,
+    first_name,
+    last_name,
+    bio,
+    gender,
+    profile_picture_url,
+    role,
+  } = req.body;
+  const hashedPassword = crypto
+    .createHmac("sha256", process.env.HASH_KEY)
+    .update(password) // Use the provided password
+    .digest("base64");
+  try {
+    const verifiedToken = jwt.verify(token, process.env.TOKEN_HASH_KEY);
+    if (verifiedToken) {
+      const { rows } = await pool.query("SELECT * FROM users WHERE id = $1;", [
+        verifiedToken.id,
+      ]);
+      let currentValues = rows[0];
+      currentValues.password = currentValues.password.trim();
+      let newValues = {
+        id: verifiedToken.id,
+        username: username ? username : null,
+        email: email ? email : null,
+        password: hashedPassword.trim(" "),
+        first_name: first_name ? first_name : null,
+        last_name: last_name ? last_name : null,
+        bio: bio ? bio : null,
+        gender: gender ? gender : null,
+        profile_picture_url: profile_picture_url ? profile_picture_url : null,
+        role: role ? role : null,
+      };
+      let updatedValues = {};
+      Object.keys(newValues).forEach((key) => {
+        const value = newValues[key];
+        if (value !== currentValues[key]) {
+          if (newValues[key] !== null && newValues[key] !== undefined) {
+            updatedValues[key] = newValues[key];
+          }
+        }
+      });
+      console.log(newValues);
+      if (Object.keys(updatedValues).length > 0) {
+        await updateUsers(
+          newValues.username,
+          newValues.email,
+          hashedPassword.trim(" "),
+          newValues.first_name,
+          newValues.last_name,
+          newValues.bio,
+          newValues.gender,
+          newValues.profile_picture_url,
+          newValues.role,
+          newValues.id
+        );
+        res.json({
+          message: "Accepted, changed values.",
+          previousData: currentValues,
+          newData: newValues,
+          updatedData: updatedValues,
+        });
+      } else {
+        res.json({
+          message: "No changes found",
+          previousData: currentValues,
+          newData: newValues,
+          updatedData: updatedValues,
+        });
+      }
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(`Server error: ${err.message}`);
+  }
+});
+//
+
 // USERS: GET
 
 app.get("/users", async (req, res) => {
@@ -80,10 +187,10 @@ app.post("/register", async (req, res) => {
       .createHmac("sha256", process.env.HASH_KEY)
       .update(password) // Use the provided password
       .digest("base64");
-    const newUser = await InsertIntoUsers(
+    const newUser = await insertIntoUsers(
       username,
       email,
-      hashedPassword,
+      hashedPassword.trim(" "),
       first_name,
       last_name,
       bio,
@@ -121,9 +228,14 @@ app.post("/login", async (req, res) => {
       return res.status(401).json({ message: "Invalid username" });
     }
     if (user.password.trim() === hashedPassword) {
-      const token = jwt.sign({ user: email }, process.env.TOKEN_HASH_KEY, {
-        expiresIn: "24h",
-      });
+      const user_id = user.id;
+      const token = jwt.sign(
+        { user: email, id: user_id },
+        process.env.TOKEN_HASH_KEY,
+        {
+          expiresIn: "24h",
+        }
+      );
       res.json({
         message: `Authorized, logged in with ${email}.`,
         accessToken: token,
@@ -142,12 +254,12 @@ app.post("/login", async (req, res) => {
 
 app.post("/addiction", async (req, res) => {
   const token = req.headers.authorization.split(" ")[1];
-  const { user_id, title, description, money_saved_per_month } = req.body;
+  const { title, description, money_saved_per_month } = req.body;
   try {
     const verifiedToken = jwt.verify(token, process.env.TOKEN_HASH_KEY);
     if (verifiedToken) {
-      const newAddictionItem = await InsertIntoAddictions(
-        user_id,
+      const newAddictionItem = await insertIntoAddictions(
+        verifiedToken.id,
         title,
         description,
         money_saved_per_month
@@ -172,7 +284,9 @@ app.get("/addictions", async (req, res) => {
   const token = req.headers.authorization.split(" ")[1];
   try {
     const verifiedToken = jwt.verify(token, process.env.TOKEN_HASH_KEY);
+    console.log(verifiedToken.user_id);
     if (verifiedToken) {
+      console.log(verifiedToken.user);
       const result = await pool.query(
         "SELECT * FROM addiction_items WHERE user_id = $1",
         [user_id]
@@ -190,8 +304,65 @@ app.get("/addictions", async (req, res) => {
 });
 
 // FUNCTIONS
+async function updateUsers(
+  username,
+  email,
+  hashedPassword,
+  first_name,
+  last_name,
+  bio,
+  gender,
+  profile_picture_url,
+  role,
+  user_id
+) {
+  const query = `
+    UPDATE users
+    SET username = $1,
+        email = $2,
+        password = $3,
+        first_name = $4,
+        last_name = $5,
+        bio = $6,
+        gender = $7,
+        profile_picture_url = $8,
+        role = $9
+    WHERE id = $10`;
+  try {
+    const { rows } = await pool.query(query, [
+      username,
+      email,
+      hashedPassword,
+      first_name,
+      last_name,
+      bio,
+      gender,
+      profile_picture_url,
+      role,
+      user_id,
+    ]);
+    console.log(
+      username,
+      email,
+      hashedPassword,
+      first_name,
+      last_name,
+      bio,
+      gender,
+      profile_picture_url,
+      role,
+      user_id
+    );
+    console.log(rows[0]);
+    return rows[0];
+  } catch (err) {
+    console.log(err);
+    console.error(err.message);
+    throw err;
+  }
+}
 
-async function InsertIntoUsers(
+async function insertIntoUsers(
   username,
   email,
   hashedPassword,
@@ -234,7 +405,7 @@ async function InsertIntoUsers(
     throw err;
   }
 }
-async function InsertIntoAddictions(
+async function insertIntoAddictions(
   user_id,
   title,
   description,
